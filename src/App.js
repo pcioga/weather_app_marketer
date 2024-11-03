862b8e7df5a31c3226beb78fd8b2721f53e7ffa0
Pedro Cioga <pcioga@gmail.com>
2024-11-03 16:29:13 +0000

    gets user current location coordinates, city and country

--- src/App.js
+++ src/App.js
@@ -4,10 +4,64 @@ import WeatherDisplay from "./components/WeatherDisplay";
 import "./App.css";
 
 function App() {
+  // defaults to Oslo
+  const [currentLocation, setCurrentLocation] = useState({
+    latitude: 40.7128,
+    longitude: -74.006,
+    city: "New York",
+    country: "US",
+  });
+
+  const getCurrentLocation = () => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCurrentLocation((prevCurrentLocation) => ({
+            ...prevCurrentLocation,
+            latitude,
+            longitude,
+          }));
+          getLocationName(latitude, longitude);
+        },
+        (error) => {
+          console.log("Unable to get location");
+        }
+      );
+    } else {
+      console.log("Geolocation browser error");
+    }
+  };
+
+  const getLocationName = (lat, lon) => {
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    fetch(
+      `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setCurrentLocation((prevCurrentLocation) => ({
+          ...prevCurrentLocation,
+          city: data[0].name,
+          country: data[0].country,
+        }));
+      })
+      .catch((error) => console.error("Error openweather API:", error));
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
   return (
     <div>
       <Header />
-      <WeatherDisplay />
+      <WeatherDisplay
+        lat={currentLocation.latitude}
+        lon={currentLocation.longitude}
+        city={currentLocation.city}
+        country={currentLocation.country}
+      />
     </div>
   );
 }
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../logo.svg";
 
-export default function WeatherDisplay() {
+export default function WeatherDisplay(props) {
   return (
     <main>
       <div className="display--main">
-        <div className="display--city">Current City</div>
+        {props.lat} | {props.lon}
+        <div className="display--city">
+          {props.city}, {props.country}
+        </div>
         <img className="display--icon" src={logo}></img>
       </div>
     </main>
